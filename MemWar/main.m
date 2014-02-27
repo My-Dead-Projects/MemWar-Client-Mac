706b474f53cfcf6fa22a2f96024b3c555b6b99a6
anthropomorphic <code@mikefdorst.com>
2014-02-27 03:21:33 -0800

    Initial Commit

---
+++ MemWar/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MemWar
+//
+//  Created by Michael Dorst on 2/27/14.
+//  Copyright (c) 2014 Michael Dorst. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
---
+++ MemWarTests/MemWarTests.m
@@ -0,0 +1,34 @@
+//
+//  MemWarTests.m
+//  MemWarTests
+//
+//  Created by Michael Dorst on 2/27/14.
+//  Copyright (c) 2014 Michael Dorst. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MemWarTests : XCTestCase
+
+@end
+
+@implementation MemWarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
