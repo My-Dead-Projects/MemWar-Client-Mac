5f799dfca1d2e28a6b9cb92e97fb66c98c259ded
anthropomorphic <code@mikefdorst.com>
2014-03-01 16:47:25 -0800

    Load game script

--- MemWar/GameController.h
+++ MemWar/GameController.h
@@ -1,12 +1,17 @@
 #import <Foundation/Foundation.h>
 #import "CoreScene.h"
+#include <Ruby/Ruby.h>
 
 @interface GameController : NSObject {
     CoreScene *scene;
+    
+    VALUE game;
 }
 
 - (id)initWithScene:(CoreScene *)scene;
 
 - (void)initializeScene;
+
+- (void)initializeGame;
 
 @end
--- MemWar/GameController.m
+++ MemWar/GameController.m
@@ -10,7 +10,20 @@
 }
 
 - (void)initializeScene {
-    
+    [self initializeGame];
+}
+
+- (void)initializeGame {
+    RUBY_INIT_STACK;
+    ruby_init();
+    ruby_init_loadpath();
+    ruby_script("embed");
+    NSString *_bundlePath = [[NSBundle mainBundle] bundlePath];
+    const char *bundlePath = [_bundlePath fileSystemRepresentation];
+    char *gameScriptPath = malloc(strlen(bundlePath)+28);
+    snprintf(gameScriptPath, strlen(bundlePath)+28,
+             "%s/Contents/Resources/game.rb", bundlePath);
+    rb_require(gameScriptPath);
 }
 
 @end
